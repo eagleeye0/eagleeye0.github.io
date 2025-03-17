db5f4bcc339de192ab0ad6dfbe10f57b3ba61255
Akshat <aki.jain51@gmail.com>
2025-03-17 15:35:31 +0530

    Change to hashbrowser

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/home";
 import Dashboard from "./pages/partner/dashboard";
 // import Shop from "./pages/shop";
